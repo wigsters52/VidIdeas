ace58737459f23f2eb2bc04f45caa70c2deed097
Ryan Wiggins 52 <ryanwiggins52@gmail.com>
2019-09-06 21:15:27 -0400

    connected mongodb, mongoose

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+
 const app = express()
+// MAP global promise -- get rid of wraning
+mongoose.Promise = global.Promise
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vididea-dev')
+  .then(() => console.log('MongoDB connected!!'))
+  .catch(err => console.log(err))
 
 // Handlebars middlewar
 
