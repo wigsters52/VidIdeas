f7044e86e60a5fd12f4c08478062d0313d2adaf1
Ryan Wiggins 52 <ryanwiggins52@gmail.com>
2019-09-09 19:43:59 -0400

    Added server side form validation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 
 const app = express()
@@ -19,6 +20,12 @@ app.engine('handlebars', exphbs(
 ))
 app.set('view engine', 'handlebars')
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
 // index route
 
 app.get('/', (req, res) => {
@@ -35,6 +42,27 @@ app.get('/about', (req, res) => {
 // Add Idea Form
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add')
+})
+
+// Process Form
+
+app.post('/ideas', (req, res) => {
+  let errors = []
+  if (!req.body.title) {
+    errors.push({ text: 'Please add a title' })
+  }
+  if (!req.body.details) {
+    errors.push({ text: 'Please add some details' })
+  }
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      errors: errors,
+      title: req.body.title,
+      details: req.body.details
+    })
+  } else {
+    res.send('passed')
+  }
 })
 
 const port = 5000
