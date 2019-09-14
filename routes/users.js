f7df692cc6e424b7f9a76e3a564937fee0844700
unknown <ryanwiggins52@gmail.com>
2019-09-14 18:55:38 -0400

    added register form validation

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,27 @@ router.get('/register', (req, res) => {
   res.render('users/register')
 })
 
+// register form post
+
+router.post('/register', (req, res) => {
+  const errors = []
+
+  if (req.body.password != req.body.password2) {
+    errors.push({ text: 'Passwords do not match' })
+  }
+  if (req.body.password.length < 4) {
+    errors.push({ text: 'Password must be at least 4 characters' })
+  }
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    })
+  } else {
+    res.send('passed')
+  }
+})
 module.exports = router
