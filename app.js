db56fb5183317410c2ef7085719aceebffd12e68
unknown <ryanwiggins52@gmail.com>
2019-09-14 21:01:37 -0400

    login functionality done...for now

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const session = require('express-session')
 const bodyParser = require('body-parser')
+const passport = require('passport')
 const mongoose = require('mongoose')
 
 const app = express()
@@ -12,6 +13,10 @@ const app = express()
 // Load routes
 const ideas = require('./routes/ideas')
 const users = require('./routes/users')
+
+// Passport config
+
+require('./config/passport')(passport)
 
 // Map global promise - get rid of warning
 mongoose.Promise = global.Promise
@@ -50,14 +55,18 @@ app.use(
     saveUninitialized: true
   })
 )
+// passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(flash())
 
 // Global variables
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.locals.success_msg = req.flash('success_msg')
   res.locals.error_msg = req.flash('error_msg')
   res.locals.error = req.flash('error')
+  res.locals.user = req.user || null
   next()
 })
 
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
-// const passport = require('passport')
+const passport = require('passport')
 const router = express.Router()
 
 // load user model
@@ -17,6 +17,16 @@ router.get('/login', (req, res) => {
 // User Register Route
 router.get('/register', (req, res) => {
   res.render('users/register')
+})
+
+// login form POST
+
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/ideas',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next)
 })
 
 // register form post
